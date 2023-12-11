25e354ed7bb4a5fa687900799f850955a071405f
K Vivek Kumar <134326034+v-house@users.noreply.github.com>
2023-12-11 20:41:30 +0530

    Update blogPosts.tsx

--- data/blogPosts.tsx
+++ data/blogPosts.tsx
@@ -55,35 +55,4 @@ export const blogPosts = [
     images: ["/about_image.jpeg"],
     imagedescription: ["Finanzy - A financial radio for a better world."],
   },
-  {
-    id: "1",
-    slug: "1",
-    title: "Finanzy",
-    tags: ["Finanzy", "Introduction"],
-    banner: "/about_image.jpeg",
-    writer: "Finanzy",
-    writerDescription: "A financial radio for a better world.",
-    date: "October 2, 2023",
-    subTitle: "A financial radio for a better world.",
-    description: [
-      {
-        heading: "Exciting Times Ahead! 🚀",
-        subDescriptions: [
-          "I am thrilled to announce a milestone in the finance world: the Nifty 50 has reached 20,000 points! 📈🙌",
-          "This remarkable accomplishment not only demonstrates the resilience and robustness of our economy but also showcases the combined efforts, innovation, and determination of all individuals, businesses, investors, and traders who have contributed to this incredible journey.",
-          "It is a testament to the strength of the markets, the resilient spirit of our nation, and the boundless potential that lies ahead. As we find ourselves on the brink of this occasion, let us not forget that it represents more than a number displayed on a screen; it symbolizes progress, prosperity, and infinite possibilities.",
-          "This milestone serves as a reminder that in the realm of finance, there are no limits – it merely marks the start of something. Let us remain dedicated to making investment choices, nurturing innovation, and cultivating an environment where opportunities continue to flourish.",
-        ],
-      },
-      {
-        heading: "Conclusion",
-        subDescriptions: [
-          "It's no longer a dream. It's history from where the real game begins. 📈📈",
-          "Congratulations to all those who have played a role in this journey!",
-        ],
-      },
-    ],
-    images: ["/about_image.jpeg"],
-    imagedescription: ["Finanzy - A financial radio for a better world."],
-  },
 ];
