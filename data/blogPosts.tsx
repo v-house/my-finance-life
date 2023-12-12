549700c2705ff04558608c7b8ebdc875db0bd113
Vivek <365anu65@gmail.com>
2023-12-12 10:07:37 +0530

    4 blogs

--- data/blogPosts.tsx
+++ data/blogPosts.tsx
@@ -55,4 +55,60 @@ export const blogPosts = [
     images: ["/about_image.jpeg"],
     imagedescription: ["Finanzy - A financial radio for a better world."],
   },
+  {
+    id: "650bd8c4972fdad1309ab167ft",
+    slug: "650bd8c4297fdad1309ab167ft",
+    title: "Muharat Trading",
+    tags: ["Finanzy", "Event", "November 12, 2023"],
+    banner: "/muharattrading.jpeg",
+    writer: "Finanzy",
+    writerDescription: "A financial radio for a better world.",
+    date: "October 28, 2023",
+    subTitle:
+      "The much-awaited Muharat Trading for 2023 is just around the corner, scheduled for November 12 - It starts at 6 PM and ends at 7.15 PM, which Includes a 15-minute pre-market session.",
+    description: [
+      {
+        heading: "About Event",
+        subDescriptions: [
+          "Muharat Trading, a symbolic ritual in the Indian financial market, is a special trading session on the day of Diwali, the festival of lights. Traders and investors participate in this session with enthusiasm, hoping to initiate trades that will bring prosperity and wealth in the upcoming year.",
+          "During the last two Mahurat Trading sessions, the stock markets have closed in the green on the Muhurat trading day. Both the Sensex and the Nifty gained 0.88 percent each on Muhurat trading in 2022, while in 2021, the two benchmarks moved up by 0.49 percent each.",
+          "As we approach the 2023 Muharat Trading on November 12, investors to maintain a balanced approach and focus on long-term investment strategies.",
+          "Stay tuned for our post-trading analysis, where we will delve into the highlights and key takeaways from the 2023 Muharat Trading session.",
+        ],
+      },
+    ],
+    images: ["/muharattrading.jpeg"],
+    imagedescription: ["Muharat Trading | Finanzy"],
+  },
+  {
+    id: "650bd8c4972fdad1309ab167f",
+    slug: "650bd8c4927fdad1309ab167f",
+    title: "Finanzy",
+    tags: ["Finanzy", "Introduction"],
+    banner: "/about_image.jpeg",
+    writer: "Finanzy",
+    writerDescription: "A financial radio for a better world.",
+    date: "October 2, 2023",
+    subTitle: "A financial radio for a better world.",
+    description: [
+      {
+        heading: "Exciting Times Ahead! 🚀",
+        subDescriptions: [
+          "I am thrilled to announce a milestone in the finance world: the Nifty 50 has reached 20,000 points! 📈🙌",
+          "This remarkable accomplishment not only demonstrates the resilience and robustness of our economy but also showcases the combined efforts, innovation, and determination of all individuals, businesses, investors, and traders who have contributed to this incredible journey.",
+          "It is a testament to the strength of the markets, the resilient spirit of our nation, and the boundless potential that lies ahead. As we find ourselves on the brink of this occasion, let us not forget that it represents more than a number displayed on a screen; it symbolizes progress, prosperity, and infinite possibilities.",
+          "This milestone serves as a reminder that in the realm of finance, there are no limits – it merely marks the start of something. Let us remain dedicated to making investment choices, nurturing innovation, and cultivating an environment where opportunities continue to flourish.",
+        ],
+      },
+      {
+        heading: "Conclusion",
+        subDescriptions: [
+          "It's no longer a dream. It's history from where the real game begins. 📈📈",
+          "Congratulations to all those who have played a role in this journey!",
+        ],
+      },
+    ],
+    images: ["/about_image.jpeg"],
+    imagedescription: ["Finanzy - A financial radio for a better world."],
+  },
 ];
