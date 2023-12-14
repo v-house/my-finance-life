bc6e8d172c8a9b4e86cd5e82904aeb2c473c77f3
Vivek <365anu65@gmail.com>
2023-12-14 19:25:57 +0530

    Return response is required.

--- pages/api/comments/[blog].js
+++ pages/api/comments/[blog].js
@@ -13,9 +13,9 @@ export default async function handler(req, res) {
         .find({ tag: blog })
         .toArray();
       // console.log(comments);
-      res.status(200).json(comments);
+      return res.status(200).json(comments);
     } catch (e) {
-      res.status(200).json({});
+      return res.status(200).json({});
       // res.status(500).json({ message: "Internal server error" });
       // console.log(e);
     }
