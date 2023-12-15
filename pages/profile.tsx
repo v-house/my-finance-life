bd9abac74e8d8998b2ceac067fd7d6279d2c2765
Vivek <365anu65@gmail.com>
2023-12-15 11:49:51 +0530

    Profile loading...

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -26,7 +26,11 @@ const Profile = () => {
   }, []);
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+      </div>
+    );
   }
 
   return (
