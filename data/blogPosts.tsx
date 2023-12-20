2612f1ab98013e4c09c0503a79c818aab911591f
Vivek <365anu65@gmail.com>
2023-12-20 18:15:23 +0530

    3rd blog image...

--- data/blogPosts.tsx
+++ data/blogPosts.tsx
@@ -57,70 +57,62 @@ export const blogPosts = [
     imagedescription: ["Finanzy - A financial radio for a better world."],
   },
   {
-  "id": "3",
-  "slug": "3",
-  "title": "Nifty weightage",
-  "tags": [
-    "Nifty 50",
-    "NSE Nifty",
-    "Index",
-    "Stock Market",
-    "Benchmark"
-  ],
-  "banner": "https://lh3.googleusercontent.com/pw/ABLVV861sZ35gSFV8IfMuQmWHsCe3sSIwV6U20_QSfeTKfcfxUI9_XwMYmUkPJeIgL70LFIrccC3vHG7aRFatIVm9KPKMobSGrWpEK4KpqlUvwpzI97jyJaJAi1llWEJLxa_IZEKEMShd_0gqZE1Fztv_vrU7cjS204vm4NcS0IByGWF3F5awsm2POBS9Nd_acHHNTguR33lgTbbTm14SHus1Iy8_wQouROXKW3pPovxLNozkWejj7vmHIJshv9XeKc4nr9PmY0xsfrJ2wp5KprsDn21uqpqjgGaHuhJ6BBZtlphP-CWqtVW85-pZe_zOitmGrJ8C4p8MhUitL-OuZAX2m364BzCjNoF94ZMsSHU1RNU3hQ0NyHLEFG9IyeKonmp9wulpYtigCyy7KSC9iIBux8VRyi_g9DHc3DPzGa9xlGNh28KykuhuP2Ic6escOe4ONn0lW3nleEITB5SZUFamv-9V72pQB-k8XnY7BH_ZfIuyC6kaKL8tR-r8yjXTrMfjxw3s7D-vNnpT4BNc0kogeC9kykzqey5uuUHPMy9ZbVJ-qZc7akgLXluKNy39BGE8yvMGo_kNRxtotpiPCc1Kf9x0yNYVU0p6TXQz3rIz7qNjrx8i5rXztS77XZyK3485hEeUiSoz433C7EnDRXFMPewdNhd5L24PXEN8kOpHrZ3jwRAyDG2JTLHYJnMVvj6lUWZZVeHwYXb6hlQOAiRDxxFiPgj2O9-CQ7PqeiMU7U3QeQ0TOq44E5UaHJvj9Cssk-9Ffv9Y_RfnTzMB1AKrxdN7fI2nb1L-XMWw4zCSqkzYrPXgse4JgvmNOc6jpDa7eqTeXtK-SEk2nuChyX-fOh6bAw_TU4XwLbMnXWtD7e-tHU926TMhuP9M7YFxQssAxwJy-42TwTFRu1niHvHq09Q-9WCZfzffz5xw8eE3knlGtofrInLBR-qnQ17qYvOAKBgNQRBN6Xx7lLeGt2UXLIC8hrD50FSlXwYoB2FPdXguMp7=w1080-h1080-s-no?authuser=3",
-  "writer": "Finanzy",
-  "writerDescription": "Welcome to our finanzy newsletter family. We wish you unforgettable learning moments - Let's start growing & earning together. Happy Investing :)",
-  "date": "20 Dec 2023",
-  "subTitle": "Let’s decode the Influencer’s of the world’s largest Index - NIFTY 50.",
-  "description": [
-    {
-      "heading": "",
-      "subDescriptions": [
-        "Nifty 50 is a stock market index in India that represents the weighted average of 50 of the largest and most liquid Indian companies listed on the National Stock Exchange (NSE). Each stock's weightage in the Nifty 50 index is determined by its market capitalization, which is the total market value of a company's outstanding shares."
-      ]
-    },
-    {
-      "heading": "Insights about NIFTY 50:",
-      "subDescriptions": [
-        "Launch Date - November 03, 1995\nReal-Time = April 22, 1996\nBase Value = 1000\nNo. of Constituents = 50\nMethodology = Real-Time\nIndex Rebalancing = Semi-Annually"
-      ]
-    },
-    {
-      "heading": "Sector Constituents:",
-      "subDescriptions": [
-        "Financial Services, Information Technology, Oil and Gas & Consumable Fuels, Fast Moving Consumer Goods, Automobile and Auto Components, Construction, Healthcare, Metals & Mining, Consumer Durables, Telecommunication, Power, Construction Materials, Services, Chemicals"
-      ]
-    },
-    {
-      "heading": "Index Methodology:",
-      "subDescriptions": [
-        "The methodology governing the computation of Nifty 50's weightage is a critical aspect that underpins the index's construction. Exploring the intricacies of the index methodology, including factors such as market capitalization, free-float market capitalization, and stock liquidity, offers a deeper understanding of how the index responds to changes in constituent stocks and sectors. This understanding can guide investors in interpreting the index's movements and formulating investment strategies aligned with the index's construction principles."
-      ]
-    },
-    {
-      "heading": "Impact on Investment Strategies:",
-      "subDescriptions": [
-        "The weightage adjustments in Nifty 50 can significantly impact investment strategies, sectoral allocations, and portfolio rebalancing for market participants. By evaluating how these changes influence investment decisions and risk management approaches, investors can adapt their strategies to align with the evolving market dynamics and optimize their investment portfolios for potential opportunities and risks."
-      ]
-    },
-    {
-      "heading": "Market Sentiment Barometer:",
-      "subDescriptions": [
-        "The weightage dynamics of the Nifty 50 serve as a barometer of market sentiment and confidence in specific stocks and sectors. Analyzing the rationale behind the weightage changes provides valuable insights into investor behavior, market trends, and the broader economic outlook. It also offers a glimpse into the prevailing market sentiment and the underlying factors driving investor preferences."
-      ]
-    },
-    {
-      "heading": "Global Comparative Analysis:",
-      "subDescriptions": [
-        "Comparing the weightage trends in Nifty 50 with global indices such as the S&P 500 or FTSE 100 can provide a broader perspective on the relative positioning of Indian equities globally. This comparative analysis offers insights into the performance of Indian equities vis-à-vis global peers, shedding light on the interconnectedness of global and domestic market forces."
-      ]
-    }
-  ],
-  "images": [
-    "https://lh3.googleusercontent.com/pw/ABLVV87_hbth6loX9fmXTcltF1ypBSY8XWs77YWdhQqnGs-Ckxh_OvzlOfBoSNqSmMKI2Mqk310srjvSNq0_rX_smCM8Rmc4qGk7jA8m4clcd-tDOvGDWm2s7CR1gGy815HwEtbu8-r-vy5skX14h2Cm0iRttchsXf_HtL6MnOcC1qhaZYmGCcU-xYn-lHM8pNqtFbjC4G2g0rZS2TzeFRiVpQfQ4nS0WWDEQZXtCqK52elBbJ5EAO2z2GsiUUb-BUIKeB9cWo3Y5p_8-iKezxat3HICz6SIjxj9iIXebKbqfLSmGuvVTe_-_1ConK8_kwzOQ5iJRliyohHX0clin2WiZQTtYVTGYPi882La_t13IIinun3PTBNqaGoLWrYnfnTJfgTtGeSQgwAMzwYw8fuhYYzWAX8LUVdJcqdQH0BI5T5Ej5FTBtrFJ7QawRaP2lzwOodfQmYEb1w9-nJFzTFycuLcSpCsaNdDJ9Mt3QfiLDWWSgOwJ2LrhchKFHKNtX-6Kx68R6Bw8xizy3tGSlJiTkWMgM2THZOsJJM9_tEsUx3WME7GxfEQHd2t7jL6zAtTvbx3ulug2V-aiBFqtNh5JvMyq9vUaAN_StRKbPcvYnVRTv50MS_CvuOe2U7RWi-TS1abKXl62MoYfsZDiZWRxLN1pPQ_dddPEgcnqhgXmUcG32NYJhFqxG-Q3oy8nByhHf-fvr7Iqc6FLXexM5371q7505_3bts3q80qPmcr16Gh0HbKjerNQsXPIsRKbHyJoFLeFaNGshmQnFLl3-LdpYXMtld6hUk-Rxh9wfoVg1gXIppA20nX2rDuvqnF_1WrhsRvt4VzAO4uFILC4nV9OLtz95SB5ory2UFHComNVxAMeB_HgtF2JTpxN9wtA3CfmF3pE_JA23Gmc5ikEA4PQRd62jQLUGQMFFwrNfRjTOMpl6RxMztqGFjfDCjh0-R_aDlTv5KvKuCj8e7uQ4PH-4q8hnoPaXF_2JBzvSQTrGJ7KHQD=w1080-h1080-s-no?authuser=3"
-  ],
-  "imagedescription": [
-    "Nifty 50."
-  ]
-}
+    id: "3",
+    slug: "3",
+    title: "Nifty weightage",
+    tags: ["Nifty 50", "NSE Nifty", "Index", "Stock Market", "Benchmark"],
+    banner: "/b3p1.jpeg",
+    writer: "Finanzy",
+    writerDescription:
+      "Welcome to our finanzy newsletter family. We wish you unforgettable learning moments - Let's start growing & earning together. Happy Investing :)",
+    date: "20 Dec 2023",
+    subTitle:
+      "Let’s decode the Influencer’s of the world’s largest Index - NIFTY 50.",
+    description: [
+      {
+        heading: "",
+        subDescriptions: [
+          "Nifty 50 is a stock market index in India that represents the weighted average of 50 of the largest and most liquid Indian companies listed on the National Stock Exchange (NSE). Each stock's weightage in the Nifty 50 index is determined by its market capitalization, which is the total market value of a company's outstanding shares.",
+        ],
+      },
+      {
+        heading: "Insights about NIFTY 50:",
+        subDescriptions: [
+          "Launch Date - November 03, 1995\nReal-Time = April 22, 1996\nBase Value = 1000\nNo. of Constituents = 50\nMethodology = Real-Time\nIndex Rebalancing = Semi-Annually",
+        ],
+      },
+      {
+        heading: "Sector Constituents:",
+        subDescriptions: [
+          "Financial Services, Information Technology, Oil and Gas & Consumable Fuels, Fast Moving Consumer Goods, Automobile and Auto Components, Construction, Healthcare, Metals & Mining, Consumer Durables, Telecommunication, Power, Construction Materials, Services, Chemicals",
+        ],
+      },
+      {
+        heading: "Index Methodology:",
+        subDescriptions: [
+          "The methodology governing the computation of Nifty 50's weightage is a critical aspect that underpins the index's construction. Exploring the intricacies of the index methodology, including factors such as market capitalization, free-float market capitalization, and stock liquidity, offers a deeper understanding of how the index responds to changes in constituent stocks and sectors. This understanding can guide investors in interpreting the index's movements and formulating investment strategies aligned with the index's construction principles.",
+        ],
+      },
+      {
+        heading: "Impact on Investment Strategies:",
+        subDescriptions: [
+          "The weightage adjustments in Nifty 50 can significantly impact investment strategies, sectoral allocations, and portfolio rebalancing for market participants. By evaluating how these changes influence investment decisions and risk management approaches, investors can adapt their strategies to align with the evolving market dynamics and optimize their investment portfolios for potential opportunities and risks.",
+        ],
+      },
+      {
+        heading: "Market Sentiment Barometer:",
+        subDescriptions: [
+          "The weightage dynamics of the Nifty 50 serve as a barometer of market sentiment and confidence in specific stocks and sectors. Analyzing the rationale behind the weightage changes provides valuable insights into investor behavior, market trends, and the broader economic outlook. It also offers a glimpse into the prevailing market sentiment and the underlying factors driving investor preferences.",
+        ],
+      },
+      {
+        heading: "Global Comparative Analysis:",
+        subDescriptions: [
+          "Comparing the weightage trends in Nifty 50 with global indices such as the S&P 500 or FTSE 100 can provide a broader perspective on the relative positioning of Indian equities globally. This comparative analysis offers insights into the performance of Indian equities vis-à-vis global peers, shedding light on the interconnectedness of global and domestic market forces.",
+        ],
+      },
+    ],
+    images: ["/b3p1.jpeg"],
+    imagedescription: ["Nifty 50."],
+  },
 ];
